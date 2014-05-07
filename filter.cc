0e9c9388030a1c881f6f5944e87489e5aa6fdf6a
Asen Lekov <asenlekoff@gmail.com>
2014-05-07 21:41:45 +0300

    Implemented oveloaded operators for filter class.

--- filter.cc
+++ filter.cc
@@ -2,23 +2,34 @@
 
 Filter::Filter(const string &word) : word(word){}
 
-// return empty string for filtered line
 string Filter::apply(const string &line) {
 	return line.find(word) == string::npos ? "" : line;
 }
 
+bool Filter::operator==(const Filter &other) const {
+    return word.compare(other.word) == 0;
+}
+
+bool Filter::operator!=(const Filter &other) const {
+    return word.compare(other.word) != 0;
+}
+
+Filter& Filter::operator+=(const char &c) {
+    word += c;
+    return *this;
+}
+
+Filter& Filter::operator+=(const char* str) {
+    word += str;
+    return *this;
+}
+
 ostream& operator<<(ostream &os, const Filter &filter) {
+    os << filter.word;
     return os;
 }
 
 istream& operator>>(istream &is, Filter &filter) {
+    is >> filter.word;
     return is;
-}
-
-Filter& Filter::operator+=(const char &c) {
-    return *this;
-}
-
-Filter& Filter::operator+=(const char* std) {
-    return *this;
 }
--- main.cc
+++ main.cc
@@ -9,11 +9,21 @@ int main (int argc, char *argv[]) {
 	Filter f_chain("chain");
 	Filter world("world");
 
+    cout << "Filtering string: = [" << hello << ", " << f_chain << ", " << world << "]"<< endl;
+    cout << "Filtering strings equals = " << (hello == f_chain) << endl;
+    cout << "Filtering strings equals = " << (hello == hello) << endl;
+
+    // Testing chaining filters add
+    hello += 'o';
+    cout << "New filtering string = " << hello << endl;
+
+    hello += " woorld";
+    cout << "New filtering string = " << hello << endl;
+
 	FilterChain chain(cin, cout);
 	chain.put_filter(hello);
 	chain.put_filter(f_chain);
 	chain.put_filter(world);
     chain.filter();
-    chain.serialize("homework.bad");
 	// hello chained world
 }
